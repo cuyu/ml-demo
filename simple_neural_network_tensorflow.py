67d4f430133a209e8fa0e6c917d6bab42548e7bc
cuyu <icyarm@qq.com>
2018-10-18 09:50:48 +0800

    Better show on tensorboard

--- simple_neural_network_tensorflow.py
+++ simple_neural_network_tensorflow.py
@@ -4,17 +4,29 @@
 Construct a simple neural network using tensorflow
 
 Note:
-    1. To debug, pls uncomment line 58 and run tensorboard first, then run the python file:
+    1. To debug, pls uncomment line 70 and run tensorboard first, then run the python file:
         tensorboard \
         --logdir ~/Code/Python/ml-demo/model_simple_nn \
         --port 6006 \
         --debugger_port 6064
-    2.To see the training process,
+    2.To see the training process, also run tensorboard:
+        tensorboard \
+        --logdir ~/Code/Python/ml-demo/model_simple_nn \
+        --port 6006
+    3. The lines followed by comment `For tensorboard` are not necessary. They are just used to generate some files so
+    that we can use tensorboard to view some graphs and dashboards.
 """
 
 import tensorflow as tf
 from tensorflow.python import debug as tf_debug
 import numpy as np
+import os
+import shutil
+
+_MODEL_NAME = 'model_simple_nn'
+# Remove the model dir if exist
+if os.path.isdir(_MODEL_NAME):
+    shutil.rmtree(_MODEL_NAME)
 
 
 def learn_tensorflow_basics():
@@ -57,8 +69,10 @@ class NeuralNetworkClassifier(object):
         # Uncomment below line for debugging
         # self.sess = tf_debug.TensorBoardDebugWrapperSession(self.sess, 'localhost:6064')
 
-    def train(self, data_set, learning_rate=0.01, steps=100):
-        loss = tf.losses.hinge_loss(self.labels, self.outputs)
+    def train(self, data_set, learning_rate=0.001, steps=100):
+        with tf.name_scope('loss'):  # For tensorboard
+            loss = tf.losses.hinge_loss(self.labels, self.outputs)
+        tf.summary.scalar('loss', loss)  # For tensorboard
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
         input_data = []
@@ -70,19 +84,18 @@ class NeuralNetworkClassifier(object):
         input_data = np.array(input_data, dtype=np.float32)
         input_label = np.array(input_label, dtype=np.float32)
 
-        saver = tf.train.Saver()
+        # Merge all the summaries, but here is just one loss scalar
+        merged = tf.summary.merge_all()  # For tensorboard
+        # Save the graph and model used by tensorboard
+        file_writer = tf.summary.FileWriter(_MODEL_NAME, self.sess.graph)  # For tensorboard
 
         self.sess.run(tf.global_variables_initializer())
         for i in range(steps):
-            loss_value = self.sess.run(loss, feed_dict={
+            loss_value, summary, _ = self.sess.run([loss, merged, optimizer], feed_dict={
                 self.inputs: input_data,
                 self.labels: input_label,
             })
-            print(loss_value)
-            self.sess.run(optimizer, feed_dict={
-                self.inputs: input_data,
-                self.labels: input_label,
-            })
+            file_writer.add_summary(summary, i)  # For tensorboard
 
     def predict(self, x, y):
         return self.sess.run(self.outputs, feed_dict={
@@ -100,5 +113,18 @@ if __name__ == '__main__':
         ([2.1, -3.0], [1]),
     ]
     classifier = NeuralNetworkClassifier()
-    classifier.train(data_set)
-    classifier.predict(0, 0)
+    classifier.train(data_set, steps=5000)
+
+    import matplotlib.pyplot as plt
+    for x in range(-30, 30):
+        for y in range(-30, 30):
+            _x = x * 0.1
+            _y = y * 0.1
+            label = classifier.predict(_x, _y)
+            color = '#a1d5ed' if label > 0 else '#efaabd'
+            plt.plot(_x, _y, color, marker='*')
+    for feature, label in data_set:
+        print(classifier.predict(*feature))
+        color = 'b' if label[0] > 0 else 'r'
+        plt.plot(*feature, color + 'o')
+    plt.show()
